c5ecf173f71209dda51a673b18e914b33ccbc31a
nich2198 <128601463+nich2198@users.noreply.github.com>
2024-04-01 00:10:15 +0200

    Update Bubble.tsx

--- src/features/bubble/components/Bubble.tsx
+++ src/features/bubble/components/Bubble.tsx
@@ -41,17 +41,15 @@ export const Bubble = (props: BubbleProps) => {
         style={{
           height: bubbleProps.theme?.chatWindow?.height ? `${bubbleProps.theme?.chatWindow?.height.toString()}px` : 'calc(100% - 100px)',
           width: bubbleProps.theme?.chatWindow?.width ? `${bubbleProps.theme?.chatWindow?.width.toString()}px` : undefined,
-          transition: 'transform 200ms cubic-bezier(0, 1.2, 1, 1), opacity 150ms ease-out',
+          bottom: '80px', // This will override the .bottom-24 class
+          // ...other styles that you have...
           'transform-origin': 'bottom right',
           transform: isBotOpened() ? 'scale3d(1, 1, 1)' : 'scale3d(0, 0, 1)',
           'box-shadow': 'rgb(0 0 0 / 16%) 0px 5px 40px',
           'background-color': bubbleProps.theme?.chatWindow?.backgroundColor || '#ffffff',
           'z-index': 42424242,
         }}
-        class={
-          `fixed sm:right-5 rounded-lg w-full sm:w-[400px] max-h-[704px]` +
-          (isBotOpened() ? ' opacity-1' : ' opacity-0 pointer-events-none') +
-          (props.theme?.button?.size === 'large' ? ' bottom-24' : ' bottom-20')
+        class={`fixed sm:right-5 rounded-lg w-full sm:w-[400px] max-h-[704px] opacity-1`}
         }
       >
         <Show when={isBotStarted()}>
